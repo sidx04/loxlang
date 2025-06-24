da6155f2c15ff18ab74e0ffef05cf801eb49f792
sidx04 <siddpal04@gmail.com>
2025-06-25 00:08:26 +0530

    feat: keywords and multi-line comment support; end of lexical analysis

--- com/loxlang/lox/Scanner.java
+++ com/loxlang/lox/Scanner.java
@@ -1,7 +1,9 @@
 package com.loxlang.lox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.loxlang.lox.TokenType.*;
 
@@ -12,8 +14,28 @@ public class Scanner {
   private int current = 0;
   private int line = 1;
 
-  Scanner(String source) {
+  private static final Map<String, TokenType> keywords = new HashMap<>();
 
+  static {
+    keywords.put("and", AND);
+    keywords.put("class", CLASS);
+    keywords.put("else", ELSE);
+    keywords.put("false", FALSE);
+    keywords.put("for", FOR);
+    keywords.put("fun", FUN);
+    keywords.put("if", IF);
+    keywords.put("nil", NIL);
+    keywords.put("or", OR);
+    keywords.put("print", PRINT);
+    keywords.put("return", RETURN);
+    keywords.put("super", SUPER);
+    keywords.put("this", THIS);
+    keywords.put("true", TRUE);
+    keywords.put("var", VAR);
+    keywords.put("while", WHILE);
+  }
+
+  Scanner(String source) {
     this.source = source;
   }
 
@@ -86,6 +108,19 @@ public class Scanner {
           while (peek() != '\n' && !isAtEnd()) {
             advance();
           }
+        } else if (match('*')) {
+          // Multi-line comment: /* ... */
+          while (!(peek() == '*' && peekNext() == '/') && !isAtEnd()) {
+            advance();
+          }
+          if (isAtEnd()) {
+            Lox.error(line, "Unterminated multi-line comment.");
+            return;
+          }
+
+          // Consume the '*/' characters
+          advance();
+          advance();
         } else {
           addToken(SLASH);
         }
@@ -111,6 +146,7 @@ public class Scanner {
         }
         break;
     }
+
   }
 
   private boolean match(char expected) {
@@ -135,15 +171,15 @@ public class Scanner {
     return source.charAt(current);
   }
 
-  private void addToken(TokenType type) {
-    addToken(type, null);
-  }
-
   private char peekNext() {
     if (current + 1 >= source.length()) {
       return '\0';
     }
     return source.charAt(current + 1);
+  }
+
+  private void addToken(TokenType type) {
+    addToken(type, null);
   }
 
   private void addToken(TokenType type, Object literal) {
@@ -205,7 +241,14 @@ public class Scanner {
     while (isAlphaNumeric(peek())) {
       advance();
     }
-    addToken(IDENTIFIER);
+
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+
+    if (type == null) {
+      type = IDENTIFIER;
+    }
+    addToken(type);
   }
 
 }
